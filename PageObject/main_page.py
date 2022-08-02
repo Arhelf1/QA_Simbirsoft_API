d8d8933ba32fc3f092fef17eb9c1f672b633a363
Arhelf1 <arhelf1@gmail.com>
2022-08-02 11:43:42 +0300

    Fix nesting
    API Яндекса позволяет получить только файлы из текущей директории
    Добавлена рекурсия для получения вложенных папок и файлов на диске

--- PageObject/main_page.py
+++ PageObject/main_page.py
@@ -4,17 +4,19 @@ import requests
 
 class MainPage():
 
-    def get_resourses(self, url_, header, mass):
-        print()
+    def get_resourses(self, url_, header, mass, lis, path="%2f", lvl=0):
+        if lvl == 0:
+            print('Список файлов и папок:')
         response = requests.request(
-            method="GET", url=f"{url_}/v1/disk/resources?path=%2F", headers=header)
+            method="GET", url=f"{url_}/v1/disk/resources?path=" + path, headers=header)
         data = json.loads(response.text)
-        l = []
         data = data['_embedded']['items']
-        print('Список файлов и папок:')
         for element in data:
-            print('   '+element['name'])
-            l.append(element['name'])
-        l == mass
-
+            print(lvl*'   '+element['name'])
+            lis.append(element['name'])
+            if "." not in element['name']:
+                self.get_resourses(
+                    url_, header, mass, lis, path+element['name']+'%2f', lvl+1)
+            # print(lis)
+        # lis == mass
 # TODO Разбить метод еще на несколько методов
--- PageObject/test_api_yandex.py
+++ PageObject/test_api_yandex.py
@@ -2,12 +2,12 @@ from main_page import MainPage
 
 
 def test_api():
-    api = "AQAAAABjef9BAADLW-8eWg6Ogk8YqZewxu36Pxc"
+    api = "AQAAAABjhMxtAADLWzXqdww540azlhMPfD7nyss"
     url = "https://cloud-api.yandex.net"
     header = {'Content-Type': 'application/json',
               'Accept': 'application/json', 'Authorization': f'OAuth {api}'}
-    mass = ['Новая папка', 'Файлы для новой папки', 'Горы.jpg', 'Зима.jpg', 'Мишки.jpg', 'Море.jpg',
-            'Москва.jpg', 'Санкт-Петербург.jpg', 'Файл для копирования.docx', 'Хлебные крошки.mp4']
-
+    mass = ['Files', 'New', 'NEW', 'Горы.jpg', 'Зима.jpg', 'Мишки.jpg', 'Море.jpg', 'Москва.jpg', 'Санкт-Петербург.jpg', 'Новая презентация.pptx', 'Новая таблица.xlsx',
+            'Новый документ.docx', 'Горы.jpg', 'Зима.jpg', 'Мишки.jpg', 'Море.jpg', 'Москва.jpg', 'Санкт-Петербург.jpg', 'Файл для копирования.docx', 'Хлебные крошки.mp4']
+    l = []
     page = MainPage()
-    page.get_resourses(url, header, mass)
+    page.get_resourses(url, header, mass, lis=l)
