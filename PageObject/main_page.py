d7f306b74f08e1c862da8c367da69bcaa9ee56e7
arhelf1 <44827252+Arhelf1@users.noreply.github.com>
2022-08-04 15:25:48 +0300

    edit

--- PageObject/main_page.py
+++ PageObject/main_page.py
@@ -4,19 +4,16 @@ import requests
 
 class MainPage():
 
-    def get_resourses(self, url_, header, mass, lis, path="%2f", lvl=0):
+    def get_resourses(self, url_, header, lis, path="%2f", lvl=0):
         if lvl == 0:
             print('Список файлов и папок:')
         response = requests.request(
             method="GET", url=f"{url_}/v1/disk/resources?path=" + path, headers=header)
-        data = json.loads(response.text)
+        data = json.loads(response.json())
         data = data['_embedded']['items']
         for element in data:
             print(lvl*'   '+element['name'])
             lis.append(element['name'])
             if "." not in element['name']:
                 self.get_resourses(
-                    url_, header, mass, lis, path+element['name']+'%2f', lvl+1)
-            # print(lis)
-        # lis == mass
-# TODO Разбить метод еще на несколько методов
+                    url_, header,  lis, path+element['name']+'%2f', lvl+1)
--- PageObject/test_api_yandex.py
+++ PageObject/test_api_yandex.py
@@ -6,8 +6,6 @@ def test_api():
     url = "https://cloud-api.yandex.net"
     header = {'Content-Type': 'application/json',
               'Accept': 'application/json', 'Authorization': f'OAuth {api}'}
-    mass = ['Files', 'New', 'NEW', 'Горы.jpg', 'Зима.jpg', 'Мишки.jpg', 'Море.jpg', 'Москва.jpg', 'Санкт-Петербург.jpg', 'Новая презентация.pptx', 'Новая таблица.xlsx',
-            'Новый документ.docx', 'Горы.jpg', 'Зима.jpg', 'Мишки.jpg', 'Море.jpg', 'Москва.jpg', 'Санкт-Петербург.jpg', 'Файл для копирования.docx', 'Хлебные крошки.mp4']
     l = []
     page = MainPage()
-    page.get_resourses(url, header, mass, l)
+    page.get_resourses(url, header, l)
